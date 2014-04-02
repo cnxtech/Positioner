584c79c696218c876b4de194d4212056a6ddc727
Sam McHardy <sam@josephmark.com.au>
2014-04-02 16:23:41 +1000

    Add Readme

--- Positioner.cpp
+++ Positioner.cpp
@@ -17,50 +17,31 @@ Positioner::Positioner(int timesToVisit) {
 }
 
 /**
- *
- * Move to each position once, then move one
- * Go to closest distance position first
- *
+ * Find next best position in grid to visit given weights in pos
+ * pos - array of weights for left, middle, right, lower is better
+ * Move to each position at most _timesToVisit
  */ 
 
 int Positioner::getNextPosition(float* pos) {
-/*    
-    Serial.print("getNextPosition(");
-    Serial.print(pos[0]);
-    Serial.print(", ");        
-    Serial.print(pos[1]);
-    Serial.print(", ");         
-    Serial.print(pos[2]);  
-    Serial.println(")");
-*/    
     int i = 0;
     if (pos[2] <= pos[1] && pos[2] <= pos[0]) {
         i = 2;
     } else if (pos[1] <= pos[0] && pos[1] <= pos[2]) {
         i = 1;
     }
-//    Serial.print("i=");
-//    Serial.println(i);
     
     // can we move to this x position?
     if (_visited[i][0] + _visited[i][1] + _visited[i][2] == 3 * _timesToVisit) {
+        // if not recursively call while weighting this out of the next calculation
         pos[i] = 500.0;
-//        Serial.println("Trying next x pos");
         getNextPosition(pos);
     } else {
         // we have a position to move to
         int j = 0;
 
         do {
-            j = rand() % 3;    
-        
+            j = rand() % 3;        
         } while (_visited[i][j] == _timesToVisit);
-/*        
-        Serial.print("Visiting x:");
-        Serial.print(i);
-        Serial.print(" y:");
-        Serial.println(j);
-*/        
         // set this as visited
         _visited[i][j]++;
         
@@ -68,7 +49,6 @@ int Positioner::getNextPosition(float* pos) {
         if (_visited[0][0] + _visited[0][1] + _visited[0][2] +
             _visited[1][0] + _visited[1][1] + _visited[1][2] +
             _visited[2][0] + _visited[2][1] + _visited[2][2] == 9 * _timesToVisit) {
-            Serial.println("Visited all positions - reset");
             _resetPositions();
         }
         
@@ -76,12 +56,10 @@ int Positioner::getNextPosition(float* pos) {
     }    
 }
     
-void Positioner::_resetPositions() {
-        
+void Positioner::_resetPositions() {        
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             _visited[i][j] = 0;
         }
     }
-
 }
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Simple class to return a position in a 3x3 grid passing in weights for left, middle and right.
+
+Attempt to not visit the same position more than a configurable number of times.
